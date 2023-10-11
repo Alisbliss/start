0165d76abe24c2c9fca72bc06b9ec02a942b67fb
A <a>
2023-10-11 16:34:31 +0300

    Initial Commit

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Алеся Афанасенкова on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
